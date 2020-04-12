8885b24409bf7731b0d0efe0a9e499ea40fa42a7
DESKTOP-KHIEM\khiem <khiem.nd179624@sis.hust.edu.vn>
2020-04-12 17:01:54 +0700

    momentjs callback/promise/async-await eventloop forloop

---
+++ packages/EventLoop/callback-promise-asyncawait.js
@@ -0,0 +1,24 @@
+var main = async function () {
+    const data = await getData('Hello', data => {
+        debugger
+        console.log(data)
+        return data + 'Again'
+    })
+        .then(() => console.log("The end!"))
+    console.log("The end! after await")
+};
+const promise1 = new Promise(function (resolve, reject) {
+    resolve('Success!');
+});
+function getData(url, callback) {
+    return promise1
+        .then(res => {
+            debugger
+            return callback(url + res)
+        })
+        .then(data => {
+            debugger
+            console.log(data)
+        })
+};
+main()
---
+++ packages/NodeServer/README.md
@@ -0,0 +1,3 @@
+1. node server.js để tạo request và lắng nghe
+2. dùng browser để đọc url
+3. node client để gửi request lên server
