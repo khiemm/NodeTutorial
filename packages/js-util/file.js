0a3412282d7772461a62bd5b1f8e8f7f17841070
Khiem Nguyen <khiem.nguyen3@sotatek.com>
2021-05-27 16:18:09 +0700

    add groupTransactionsToSettlements

--- packages/js-util/file.js
+++ packages/js-util/file.js
@@ -5,6 +5,7 @@ const ObjectId = require("bson").ObjectId;
 const jsonToCsv = require("json-2-csv");
 const csvParser = require("csv-parser");
 const timeSpan = require("time-span");
+const _ = require('lodash');
 
 /**
  *
@@ -42,9 +43,10 @@ const transformCsvToJson = (
     skip_empty_lines: true,
   });
   console.log(jsonData.length);
-  // fs.writeFileSync(newPath, JSON.stringify(jsonData));
+  fs.writeFileSync(newPath, JSON.stringify(jsonData));
   console.timeEnd("lol");
 };
+// transformCsvToJson('/home/khiem/Documents/import\ transactions/260_transactions_of_166_open_batches_were_exported_from_GMS.csv', '/home/khiem/Documents/import\ transactions/123.json')
 
 const transformCsvToJsonByStream = async (filePath) => {
   let duration;
@@ -115,11 +117,6 @@ const pipeWriteStreamAppend = () => {
 //   "/home/khiem/Documents/migration/profile_ignore_Daily Gift_Card_Registration_ULP_26-27_March_2021/",
 //   '"PROFILE_NO","TITLE","NAME","NAME_DISPLAY","IC_MYKAD","IC_OTHERS","IC_PASSPORT","DATE_BIRTH","GENDER","RACE","NATIONALITY","MARITAL","HAVE_CAR","HAVE_CC","REG_DEALER","DATE_CREATE","DATE_REGISTER","DATE_ACTIVATE","MASTER_ID","CASH_ACC","CASH_AVAILABLE","CASH_LOCK","ADDRESS1","ADDRESS2","ADDRESS3","POSTCODE","CITY","STATE","COUNTRY","M_ADDRESS1","M_ADDRESS2","M_ADDRESS3","M_POSTCODE","M_CITY","M_STATE","M_COUNTRY","BIRTHDATE","EMAIL","CONTACT","MOBILE","DAILYSPENDING","MONTHLYSPENDING","SINGLELIMIT","TOPUPLIMIT","PRODUCTLIMIT","PROMO","DATE_CASH_ACC_RECON","CARD_NO"'
 // );
-// transformCsvToJson(
-//   "/home/khiem/Downloads/test_bulk_transfer/import_bulk_transfer_0123.csv"
-// );
-
-// transformCsvToJson("large-dataset.csv");
 
 // transformCsvToJsonByStream("large-dataset.csv");
 
@@ -128,3 +125,50 @@ const pipeWriteStreamAppend = () => {
 // pipeWriteStream();
 
 // pipeWriteStreamAppend();I
+
+const groupTransactionToSettlement = async (
+  filePath,
+  newPath = filePath.slice(0, -3) + "csv"
+) => {
+  console.time("lol");
+  const data = fs.readFileSync(filePath);
+  const jsonData = csv.parse(data, {
+    columns: true,
+    skip_empty_lines: true,
+  });
+  console.log(jsonData.length);
+  const arr = {}
+  const result = jsonData.reduce((acc, val) => {
+    const key = val.MID + val.TID + val.BATCH_NO
+    if (!arr[key]) {
+      arr[key] = Object.assign({}, {
+        STAN: val.STAN,
+        MTI: '',
+        NII: '',
+        TID: val.TID,
+        MID: val.MID,
+        BATCH_NO: val.BATCH_NO,
+        PURCHASE_AMT: val.TX_TYPE === 'TOPUP' ? 0 : +val.AMT_TX,
+        PURCHASE_COUNT: val.TX_TYPE === 'TOPUP' ? 0 : 1,
+        TOPUP_AMT: val.TX_TYPE === 'TOPUP' ? +val.AMT_TX : 0,
+        TOPUP_COUNT: val.TX_TYPE === 'TOPUP' ? 1 : 0,
+      })
+      acc.push(arr[key])
+    } else {
+      // ref, so arr[key] change => result[e=arr[key]] change
+      if (val.TX_TYPE === 'TOPUP') {
+        arr[key].TOPUP_AMT += +val.AMT_TX
+        arr[key].TOPUP_COUNT += 1
+      } else {
+        arr[key].PURCHASE_AMT += +val.AMT_TX
+        arr[key].PURCHASE_COUNT += 1
+      }
+    }
+    return acc
+  }, [])
+  console.log(result.length)
+  const csvData = await jsonToCsv.json2csvAsync(result);
+  fs.writeFileSync(newPath, csvData);
+  console.timeEnd("lol");
+};
+groupTransactionToSettlement('/home/khiem/Documents/import\ transactions/260_transactions_of_166_open_batches_were_exported_from_GMS.csv', '/home/khiem/Documents/import\ transactions/1234567.csv')
